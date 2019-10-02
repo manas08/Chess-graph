074fbc86c26b82b5df49fb13bfa4b683a249afa6
manas08 <kratkra1@uhk.cz>
2019-10-02 13:50:38 +0200

    Test

--- diplom/src/cz/uhk/diplom/MainWindow.java
+++ diplom/src/cz/uhk/diplom/MainWindow.java
@@ -122,7 +122,7 @@ public class MainWindow extends JFrame implements MouseListener, MouseMotionList
 				test++;
 			} 
 		}		
-		// wnfdejfnergerg
+
 		//System.out.println("=============================================");
 
 
